a537d4a4c0c132bafa3a27e8ab2d6f513e483db1
Dev-Senior82 <slayvier0@gmail.com>
2026-04-14 13:57:32 -0300

    sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,9 @@
+export default function sitemap() {
+  return [
+    { url: "https://www.elofastjob.com.br" },
+    { url: "https://www.elofastjob.com.br/elo-job" },
+    { url: "https://www.elofastjob.com.br/duo-boost" },
+    { url: "https://www.elofastjob.com.br/coach-lol" },
+    { url: "https://www.elofastjob.com.br/boost-lol" }
+  ]
+}
