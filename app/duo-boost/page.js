b27dfeb04bbf9229ed730a0cd9d1741e2555ded8
Dev-Senior82 <slayvier0@gmail.com>
2026-04-14 13:52:11 -0300

    page.js

---
+++ app/duo-boost/page.js
@@ -0,0 +1,21 @@
+export const metadata = {
+  title: "Duo Boost LoL - Suba Jogando Junto",
+  description:
+    "Duo Boost LoL com jogadores de alto elo. Suba de elo jogando com booster.",
+}
+
+export default function Page() {
+  return (
+    <div className="p-8">
+      <h1>Duo Boost LoL</h1>
+      <p>
+        Suba de elo jogando junto com booster profissional.
+        Serviço de duo boost rápido e seguro.
+      </p>
+
+      <a href="/" className="underline">
+        Calcular Duo Boost
+      </a>
+    </div>
+  )
+}
