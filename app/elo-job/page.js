eec6958cb1ce9be2dea6bf8752d778398584c773
Dev-Senior82 <slayvier0@gmail.com>
2026-04-14 16:32:46 -0300

    Update page.js

--- app/elo-job/page.js
+++ app/elo-job/page.js
@@ -1,21 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+
 export const metadata = {
   title: "Elo Job LoL - Subir de Elo Rápido e Seguro",
   description:
     "Elo Job LoL com boosters profissionais. Suba de elo rápido e seguro.",
-}
+};
 
 export default function Page() {
-  return (
-    <div className="p-8">
-      <h1>Elo Job LoL</h1>
-      <p>
-        Oferecemos serviço de elo job LoL barato, rápido e seguro.
-        Suba de elo com boosters profissionais.
-      </p>
+  useEffect(() => {
+    window.location.replace("/#calculadora");
+  }, []);
 
-      <a href="/" className="underline">
-        Calcular Elo Job
-      </a>
-    </div>
-  )
+  return null;
 }
