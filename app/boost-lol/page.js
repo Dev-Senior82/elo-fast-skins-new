960d259025aa3def34cedc4421cd414fda0e54e9
Dev-Senior82 <slayvier0@gmail.com>
2026-04-14 13:53:57 -0300

    page.js

---
+++ app/boost-lol/page.js
@@ -0,0 +1,21 @@
+export const metadata = {
+  title: "Boost LoL - Elo Boost Rápido",
+  description:
+    "Boost LoL rápido e seguro. Suba de elo com boosters profissionais.",
+}
+
+export default function Page() {
+  return (
+    <div className="p-8">
+      <h1>Boost LoL</h1>
+      <p>
+        Serviço de boost LoL profissional.
+        Suba de elo com segurança e rapidez.
+      </p>
+
+      <a href="/" className="underline">
+        Calcular Boost
+      </a>
+    </div>
+  )
+}
