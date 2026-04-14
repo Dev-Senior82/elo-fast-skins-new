d858a3efa4e9d9cb27de723746925e2a42a6f3b9
Dev-Senior82 <slayvier0@gmail.com>
2026-04-14 14:18:08 -0300

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,9 +1,34 @@
 export default function sitemap() {
   return [
-    { url: "https://www.elofastjob.com.br" },
-    { url: "https://www.elofastjob.com.br/elo-job" },
-    { url: "https://www.elofastjob.com.br/duo-boost" },
-    { url: "https://www.elofastjob.com.br/coach-lol" },
-    { url: "https://www.elofastjob.com.br/boost-lol" }
+    {
+      url: "https://www.elofastjob.com.br",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://www.elofastjob.com.br/elo-job",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: "https://www.elofastjob.com.br/duo-boost",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: "https://www.elofastjob.com.br/coach-lol",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.elofastjob.com.br/boost-lol",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    }
   ]
 }
